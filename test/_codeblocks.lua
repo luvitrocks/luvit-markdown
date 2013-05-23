bf7ef99460300bc27acbc77043e30891f6362f51
Marc Neudert <marc.neudert@gmail.com>
2013-05-23 23:10:12 +0200

    prepares codeblock parsing tests

---
+++ test/_codeblocks.lua
@@ -0,0 +1,5 @@
+return {
+  {markdown = '    First\n    Lines    \nCodeblocked', html = '<pre><code>First\nLines\nCodeblocked\n</code></pre>'},
+  {markdown = 'Intro\n\n    Code\n      With    \nIndent\n\nOutro',
+   html = '<p>Intro</p>\n\n<pre><code>Code\n  With\nIndent\n</code></pre>\n\n<p>Outro</p>'}
+}
